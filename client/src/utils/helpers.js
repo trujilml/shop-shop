394a94a3b8c7045d4a1545a03404beca3dbfd0c6
trujilml <mickeytrj@gmail.com>
2022-02-16 17:04:15 -0600

    created and set up the idb helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,58 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolver, reject) => {
+
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    let db, tx, store;
+
+    request.onupgradeneeded = function(e) {
+      const db = request.result; 
+      db.createObjectStore('products', {keyPath: '_id'});
+      db.createObjectStore('categories', {keyPath: '_id'});
+      db.createObjectStore('cart', {keyPath: '_id'});
+    };
+
+    request.onerror = function(e) {
+      console.log('There was an error');
+    };
+
+    request.onsuccess = function(e) {
+      db= request.result;
+
+      tx= db.transaction(storeName, 'readwrite');
+
+      store=tx.objectStore(storeName);
+
+      db.onerror=function(e) {
+        console.log('error', e);
+      };
+
+      switch (method) {
+        case 'put':
+          store.put(object);
+          resolver(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function() {
+            resolver(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default: 
+          console.log('No valid method');
+          break;
+      }
+
+      tx.oncomplete = function() {
+        db.close();
+      };
+    };
+  });
+}
