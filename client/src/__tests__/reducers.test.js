75a664a3d0ec667cdce66f0560f90870e1fa18b6
trujilml <mickeytrj@gmail.com>
2022-02-16 12:14:49 -0600

    finished writing and running tests for shopping cart functionality

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -81,3 +81,64 @@ test('ADD_MULTIPLE_TO_CART', () => {
     expect(newState.cart.length).toBe(4);
     expect(initialState.cart.length).toBe(2);
 });
+
+test('REMOVE_FROM_CART', () => {
+    let newState1 = reducer(initialState, {
+        type: REMOVE_FROM_CART,
+        _id: '1'
+    });
+
+    expect(newState1.cartOpen).toBe(true);
+
+    expect(newState1.cart.length).toBe(1);
+    expect(newState1.cart[0]._id).toBe('2');
+
+    let newState2 = reducer(newState1, {
+        type: REMOVE_FROM_CART,
+        _id: '2'
+    });
+
+    expect(newState2.cartOpen).toBe(false);
+    expect(newState2.cart.length).toBe(0);
+
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('UPDATE_CART_QUANTITY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CART_QUANTITY,
+        _id: '1',
+        purchaseQuantity: 3
+    });
+
+    expect(newState.cartOpen).toBe(true);
+    expect(newState.cart[0].purchaseQuantity).toBe(3);
+    expect(newState.cart[1].purchaseQuantity).toBe(2);
+
+    expect(initialState.cartOpen).toBe(false);
+});
+
+test('CLEAR_CART', () => {
+    let newState = reducer(initialState, {
+        type: CLEAR_CART
+    });
+
+    expect(newState.cartOpen).toBe(false);
+    expect(newState.cart.length).toBe(0);
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('TOGGLE_CART', () => {
+    let newState = reducer(initialState, {
+        type: TOGGLE_CART
+    });
+
+    expect(newState.cartOpen).toBe(true);
+    expect(initialState.cartOpen).toBe(false);
+
+    let newState2 = reducer(newState, {
+        type: TOGGLE_CART
+    });
+
+    expect(newState2.cartOpen).toBe(false);
+});
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -45,6 +45,42 @@ export const reducer = (state, action) => {
                     cart: [...state.cart, ...action.products],
                 };
 
+            case REMOVE_FROM_CART:
+                let newState = state.cart.filter(product => {
+                    return product._id !== action._id;
+                });
+
+                return {
+                    ...state,
+                    cartOpen: newState.length > 0,
+                    cart: newState
+                };
+            
+            case UPDATE_CART_QUANTITY:
+                return {
+                    ...state,
+                    cartOpen: true,
+                    cart: state.cart.map(product => {
+                        if (action._id === product._id) {
+                            product.purchaseQuantity = action.purchaseQuantity;
+                        }
+                        return product;
+                    })
+                };
+
+            case CLEAR_CART:
+                return {
+                    ...state,
+                    cartOpen: false,
+                    cart: []
+                };
+            
+            case TOGGLE_CART:
+                return {
+                    ...state,
+                    cartOpen: !state.cartOpen
+                };
+
             default: 
             return state;
     }
