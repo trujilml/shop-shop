f7a878a3cd09ff8d66fc97d74caa2cbc4d930928
trujilml <mickeytrj@gmail.com>
2022-02-15 20:24:11 -0600

    added first two shopping cart reducers and their respective tests

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -3,7 +3,13 @@ import { reducer } from '../utils/reducers';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from '../utils/actions';
 
 //create a sample of what our global state will look like
@@ -11,6 +17,19 @@ const initialState = {
     products: [],
     categories: [{name: 'Food'}],
     currentCategory: '1',
+    cart: [
+        {
+        _id: '1',
+        name: 'Soup',
+        purchaseQuantity: 1
+        },
+        {
+        _id: '2',
+        name: 'Bread',
+        purchaseQuantity: 2
+        }
+    ],
+    cartOpen: false
 };
 
 test('UPDATE_PRODUCTS', () => {
@@ -41,4 +60,24 @@ test('UPDATE_CURRENT_CATEGORY', () => {
 
     expect(newState.currentCategory).toBe('2');
     expect(initialState.currentCategory).toBe('1');
+});
+
+test('ADD_TO_CART', () => {
+    let newState = reducer(initialState, {
+        type: ADD_TO_CART,
+        product: { purchaseQuantity: 1}
+    });
+
+    expect(newState.cart.length).toBe(3);
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('ADD_MULTIPLE_TO_CART', () => {
+    let newState = reducer(initialState, {
+        type: ADD_MULTIPLE_TO_CART,
+        products: [{},{}]
+    });
+
+    expect(newState.cart.length).toBe(4);
+    expect(initialState.cart.length).toBe(2);
 });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,7 +3,13 @@ import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -25,6 +31,19 @@ export const reducer = (state, action) => {
                 ...state,
                 currentCategory: action.currentCategory
             };
+        
+            case ADD_TO_CART:
+                return {
+                    ...state,
+                    cartOpen: true,
+                    cart: [...state.cart, action.product]
+                };
+            
+            case ADD_MULTIPLE_TO_CART:
+                return {
+                    ...state,
+                    cart: [...state.cart, ...action.products],
+                };
 
             default: 
             return state;
