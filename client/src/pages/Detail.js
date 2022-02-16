ee6604b3f09f666112d223561c8cd3aa3040fa4c
trujilml <mickeytrj@gmail.com>
2022-02-16 16:49:39 -0600

    implemented update and delete object of items in the shopping cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -16,7 +16,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -30,9 +30,26 @@ function Detail() {
   }, [products, data, dispatch, id]);
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if(itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  }
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
 
@@ -49,7 +66,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+            disabled={!cart.find(p => p._id === currentProduct._id)}
+            onClick={removeFromCart}
+            >
+            Remove from Cart
+            </button>
           </p>
 
           <img
