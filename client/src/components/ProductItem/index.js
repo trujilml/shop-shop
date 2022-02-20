bd87c8c4b67c3a33b5b56ec763fd8f2ddb85ad7c
trujilml <mickeytrj@gmail.com>
2022-02-20 14:02:02 -0600

    implemented shopping cart storage

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { idbPromise } from "../../utils/helpers";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
@@ -18,13 +19,15 @@ function ProductItem(item) {
   const {cart} = state;
 
   const addToCart = () => {
-
     const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
@@ -32,6 +35,7 @@ function ProductItem(item) {
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1}
     });
+    idbPromise('cart', 'put', {...item, purchaseQuantity: 1});
   }
   };
 
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -52,19 +52,26 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1 }
     });
+    idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
   }
-  };
+  }
 
   const removeFromCart = () => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    idbPromise('cart', 'delete', {...currentProduct});
   };
 
   return (
